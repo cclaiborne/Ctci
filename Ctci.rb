7197649375c00fd6d4747236ae07191d209ef3c1
cclaiborne <claiborne206@gmail.com>
2015-07-13 23:42:55 -0700

    one_point_three_completed

--- Ctci.rb
+++ Ctci.rb
@@ -14,11 +14,51 @@ class Ctci
     end
     return true
   end
+
+  #Given two strings, write a method to decide if one is a permutation of the other
+  def one_point_three(s1, s2)
+    @split_s1 = s1.split(//)
+    @split_s2 = s2.split(//)
+    h1 = Hash.new
+    if (s1.length != s2.length)
+      return false
+    end
+    @split_s1.each do |i|
+      if (!h1.has_key?(i))
+        h1[i] = 1
+      else
+        @value = h1.fetch(i)
+        @value = @value + 1
+        h1[i] = @value
+      end
+    end
+    @split_s2.each do |i|
+      if (!h1.has_key?(i))
+        return false
+      else
+        @value = h1.fetch(i)
+        @value = @value - 1
+        if(@value == -1)
+          return false
+        end
+        h1[i] = @value
+      end
+    end
+    @split_s1.each do |i|
+      if (h1.fetch(i) != 0)
+        return false
+      end
+    end
+    return true
+  end
 end
 
 test = Ctci.new
 #return true
-puts test.one_point_one("abc")
+#puts test.one_point_one("abc")
 #return false
-puts test.one_point_one("abcabc")
+#puts test.one_point_one("abcabc")
+#return true
+puts test.one_point_three("abb", "bbb")
+
 
