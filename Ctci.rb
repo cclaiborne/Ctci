e104c348bce6a36d369a269ea7df53ea403291d8
cclaiborne <claiborne206@gmail.com>
2015-07-13 18:05:16 -0700

    one_point_one completed

--- Ctci.rb
+++ Ctci.rb
@@ -1,7 +1,24 @@
 class Ctci
+  require 'set'
   #Implement an algorithm to determine if a string has all unique characters.
   #What if you cannot use additional data structures?
   def one_point_one(word)
-
+    @split_word = word.split(//)
+    s1 = Set.new
+    @split_word.each do |i|
+      if (!s1.include?(i))
+        s1.add(i)
+      else
+        return false
+      end
+    end
+    return true
   end
 end
+
+test = Ctci.new
+#return true
+puts test.one_point_one("abc")
+#return false
+puts test.one_point_one("abcabc")
+
