422075739e1dad56b3767f3b15fdaee13755eabf
cclaiborne <claiborne206@gmail.com>
2015-07-13 17:51:19 -0700

    add project files

---
+++ Ctci.rb
@@ -0,0 +1,7 @@
+class Ctci
+  #Implement an algorithm to determine if a string has all unique characters.
+  #What if you cannot use additional data structures?
+  def one_point_one(word)
+
+  end
+end
