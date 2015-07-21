96ca6894b01e7b90535c800ebac3b96695711759
cclaiborne <claiborne206@gmail.com>
2015-07-21 16:54:04 -0700

    two_point_two_completed

--- Chapter2.rb
+++ Chapter2.rb
@@ -22,6 +22,24 @@ class Chapter2
     end
     return head
   end
+
+  #remove kth element from a linked list
+  #defined as k = 1 returning the last element
+  def two_point_two(head, k)
+    @current = head
+    @length = 1
+    while(@current.next != nil)
+      @length += 1
+      @current = @current.next
+    end
+    @current = head
+    for i in 0..@length-1
+      if (i == @length - k)
+        return @current
+      end
+      @current = @current.next
+    end
+  end
 end
 
 n1 = Node.new("a", nil)
@@ -34,6 +52,7 @@ n2.next = n3
 n3.next = n4
 
 c2 = Chapter2.new
-duplicates_removed = c2.two_point_one(n1)
+#duplicates_removed = c2.two_point_one(n1)
 #n3 should be removed, thus n4 is printed
-puts duplicates_removed.next.next.data
+#puts duplicates_removed.next.next.data
+puts c2.two_point_two(n1, 2).data
---
+++ Chapter9.rb
@@ -0,0 +1,16 @@
+
+class Chapter9
+  # Write a method to return all subsets of a set.
+  def nine_point_four(set)
+
+  end
+
+  #Write a method to compute all permutations of a string.
+  def nine_point_five(word)
+
+  end
+
+
+end
+
+test = Chapter9.new
